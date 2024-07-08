ddc5a71c8e15a227da311ce1d938ad72518d6cdd
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-08 23:48:56 +0500

    helloworld

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello_Sworld</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, cum maxime. Placeat sapiente at, 
+        nihil porro ad iste sit a esse modi sint assumenda deleniti minima harum nulla possimus ex.</p>
+    </div>
+  );
+}
